beecd94648dd037217cc079ae7731f1f9be0eeea
AbdullahEng <darabdullah507@gmail.com>
2022-05-09 03:12:00 +0300

    registration and login view model is done

---
+++ AdmissionSystem/AdmissionSystem/View_Model/Identity_view_model/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdmissionSystem.View_Model.Identity_view_model
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string  UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string  password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string  ConfirmPassword { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        public int TheIDnumber { get; set; }
+
+    }
+}
