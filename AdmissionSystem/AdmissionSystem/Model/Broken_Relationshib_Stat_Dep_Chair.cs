a3444e7301a67d843fc95a1a56ba9d5d245f543f
AbdullahEng <darabdullah507@gmail.com>
2022-08-02 23:24:56 +0300

    you should make migration before you make pull

--- AdmissionSystem/AdmissionSystem/Model/Broken_Relationshib_Stat_Dep_Chair.cs
+++ AdmissionSystem/AdmissionSystem/Model/Broken_Relationshib_Stat_Dep_Chair.cs
@@ -8,9 +8,9 @@ namespace AdmissionSystem.Model
     public class Broken_Relationshib_Stat_Dep_Chair
     {
         public int id { get; set; }
-      //  public int FK_statues_Of_Admission_EligibiltyId { get; set; }//one_to_many relationship
+       public int FK_statues_Of_Admission_EligibiltyId { get; set; }//one_to_many relationship
         public Statues_of_admission_eligibilty FK_statues_Of_Admission_Eligibilty { get; set; }//one_to_many relationship
-     //   public int Fk_departmentId { get; set; }//one_to_many relationship
+       public int Fk_departmentId { get; set; }//one_to_many relationship
         public Department Fk_department { get; set; }//one_to_many relationship
         public int Chair_count { get; set; }
     }
